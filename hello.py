ea51a0d89d779cacb02fd4800615d245975914df
An Chu <anchu@outlook.com>
2013-11-21 20:13:08 +0700

    For loop example

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@
 # date: 21/11/2013
 
 print "hello world"
+
+my_fav_langs = ["Python", "R", "Markdown", "LaTeX", "Git"]
+for lang in my_fav_langs:
+    print "My favorite programming language is:", lang
+    
+
